df0abdbadd4448aad689b15f896c739fd64ad9ee
yanchi03 <131846489+yanchi03@users.noreply.github.com>
2023-04-26 13:52:04 +0800

    Add files via upload

---
+++ SC001_lecture01/StepUp.py
@@ -0,0 +1,21 @@
+"""
+File: StepUp.py
+Name: TODO:
+------------------------
+This file shows Karel picking up 
+the beeper at Street 1 Avenue 2,
+putting it onto Street 2 Avenue 4.
+Karel will be facing East at Street
+2 Avenue 5 at the end of this program.
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    pass
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
