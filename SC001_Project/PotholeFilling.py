25169763cc92ac2400ee7038aa626de3ad62086d
yanchi03 <131846489+yanchi03@users.noreply.github.com>
2023-04-26 14:04:52 +0800

    Add files via upload

---
+++ SC001_Project/PotholeFilling.py
@@ -0,0 +1,22 @@
+"""
+File: PotholeFilling.py
+Name: TODO:
+--------------------------
+This program shows karel filling 3
+potholes. Students learn the concept of
+decomposition through the process.
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    TODO:
+    """
+    pass
+
+
+# ----- DO NOT EDIT CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
